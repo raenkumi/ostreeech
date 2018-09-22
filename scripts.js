87c1820d7fbee4c6060a94c59f196809d91d6297
Daniel Calonico <calonicodaniel@gmail.com>
2018-09-22 11:29:06 -0300

    upd 0922

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// SCRIP PARA O QUIZ //
+
+quizEntries(); {
+    var a = document.getElementsByname("a.incubacao").value;
+    var b = document.getElementsByname("b.corrnamea").value;
+    var c = document.getElementsByname("c.estomago").value;
+    var d = document.getElementsByname("d.origem").value;
+    var e = document.getElementsByname("e.ovo").value;
+    var f = document.getElementsByname("f.dentes").value;
+    var g = document.getElementsByname("g.altura").value;
+    var h = document.getElementsByname("h.peso").value;
+    var i = document.getElementsByname("i.ataque").value;
+    var j = document.getElementsByname("j.celta").value;
+
+    console.log(a, b, c, d, e, f, g, h, i, j);
+}
