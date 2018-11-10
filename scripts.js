7845a34a432a89f845a3c57e14db2b0770a84bc3
Daniel Calonico <calonicodaniel@gmail.com>
2018-11-10 19:33:39 -0200

    upd 1011

--- scripts.js
+++ scripts.js
@@ -1,18 +1,10 @@
-// SCRIP PARA O QUIZ //
+// 2 - MUDANÇA DE COR QUANDO O MOUSE PASSA POR CIMA DE UM PARÁGRAFO
 
-// CÓDIGO NÃO TEM FUNÇÃO! //
 
-function quizEntries() {
-    var a = document.getElementsByTagName("a.incubacao").value;
-    var b = document.getElementsByTagName("b.corrnamea").value;
-    var c = document.getElementsByTagName("c.estomago").value;
-    var d = document.getElementsByTagName("d.origem").value;
-    var e = document.getElementsByTagName("e.ovo").value;
-    var f = document.getElementsByTagName("f.dentes").value;
-    var g = document.getElementsByTagName("g.altura").value;
-    var h = document.getElementsByTagName("h.peso").value;
-    var i = document.getElementsByTagName("i.ataque").value;
-    var j = document.getElementsByTagName("j.celta").value;
 
-    console.log(a, b, c, d, e, f, g, h, i, j);
-}
+// 3 - ENFATIZAR A POSIÇÃO DO CURSOR
+
+
+
+// 4 - TEXTO E IMAGEM QUE APARECEM DE ACORDO COM O DIA DA SEMANA
+
