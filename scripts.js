f1475beb107823ead715f87dd289e8ed779e9681
Daniel Calonico <calonicodaniel@gmail.com>
2018-11-29 09:25:30 -0200

    upd final

--- scripts.js
+++ scripts.js
@@ -5,22 +5,22 @@ const imageDateDisplay = () => {
         case 0:
             document.getElementById("headerimg").src = "pics/running0.jpg";
             break;
-        case 1: 
+        case 1:
             document.getElementById("headerimg").src = "pics/running1.jpg";
             break;
-        case 2: 
+        case 2:
             document.getElementById("headerimg").src = "pics/running2.jpg";
             break;
-        case 3: 
+        case 3:
             document.getElementById("headerimg").src = "pics/running3.jpg";
             break;
-        case 4: 
+        case 4:
             document.getElementById("headerimg").src = "pics/running4.jpg";
             break;
-        case 5: 
+        case 5:
             document.getElementById("headerimg").src = "pics/running5.jpg";
             break;
-        case 6: 
+        case 6:
             document.getElementById("headerimg").src = "pics/running6.jpg";
             break;
         }
@@ -30,6 +30,7 @@ const imageDateDisplay = () => {
 
 const quizEntries = () => {
     let fullyAnswered = false;
+    let correctAnswerCount = 0;
     let answers = [
         [
             document.getElementById('quizanswera1').checked,
@@ -82,45 +83,138 @@ const quizEntries = () => {
             document.getElementById('quizanswerj3').checked,
         ]
     ];
-
-    for (i = 0; i < answers.length; i++){
-        if (answers[i].includes(true)) {
-            fullyAnswered = true;
-        }
+    // checar se todas as perguntas foram respondidas
+    if (answers[0].includes(true)
+        && answers[1].includes(true) 
+        && answers[2].includes(true)
+        && answers[3].includes(true)
+        && answers[4].includes(true)
+        && answers[5].includes(true)
+        && answers[6].includes(true) 
+        && answers[7].includes(true)
+        && answers[8].includes(true)
+        && answers[9].includes(true)
+    ) {
+        fullyAnswered = true;
     }
-
-    if (fullyAnswered) {
+    // correcao das respostas
+    if (!fullyAnswered) {
+        document.getElementById('angryboi').innerHTML = "<b>Responda todas as questões!</b>";
+        document.getElementById('angryboi').style = "display:block; color:red"
+    } else if (fullyAnswered) {
         if (answers[0].includes(true)){
-            if (answers[0][1])  {
-                document.getElementById('quizbox0').style = "border: solid 2px green";
-                document.getElementById('quizboxa2').style = "background-color: none";
+            if (answers[0][1]) {
+                correctAnswerCount++;
+                document.getElementById('quizbox0').style = "box-shadow:0px 0px 5px #00cc99";
+                document.getElementById('quizboxa2').style = "background-color:none";
             } else if (!answers[0][1]) {
-                document.getElementById('quizbox0').style = "border: solid 2px red";
-                document.getElementById('quizboxa2').style = "background-color: green";
+                document.getElementById('quizbox0').style = "box-shadow:0px 0px 0px 2px red";
+                document.getElementById('quizboxa2').style = "background-color:#00cc99; color:white";
             }
         }
-
+        if (answers[1].includes(true)){
+            if (answers[1][2]) {
+                correctAnswerCount++;
+                document.getElementById('quizbox1').style = "box-shadow:0px 0px 5px #00cc99";
+                document.getElementById('quizboxb3').style = "background-color:none";
+            } else if (!answers[1][2]) {
+                document.getElementById('quizbox1').style = "box-shadow:0px 0px 0px 2px red";
+                document.getElementById('quizboxb3').style = "background-color:#00cc99; color:white";
+            }
+        }
+        if (answers[2].includes(true)){
+            if (answers[2][0]) {
+                correctAnswerCount++;
+                document.getElementById('quizbox2').style = "box-shadow:0px 0px 5px #00cc99";
+                document.getElementById('quizboxc1').style = "background-color:none";
+            } else if (!answers[2][0]) {
+                document.getElementById('quizbox2').style = "box-shadow:0px 0px 0px 2px red";
+                document.getElementById('quizboxc1').style = "background-color:#00cc99; color:white";
+            }
+        }
+        if (answers[3].includes(true)){
+            if (answers[3][0]) {
+                correctAnswerCount++;
+                document.getElementById('quizbox3').style = "box-shadow:0px 0px 5px #00cc99";
+                document.getElementById('quizboxd1').style = "background-color:none";
+            } else if (!answers[3][0]) {
+                document.getElementById('quizbox3').style = "box-shadow:0px 0px 0px 2px red";
+                document.getElementById('quizboxd1').style = "background-color:#00cc99; color:white";
+            }
+        }
         if (answers[4].includes(true)){
-            if (answers[4][1])  {
-                document.getElementById('quizbox4').style = "border: solid 2px green";
-                document.getElementById('quizboxe2').style = "background-color: none";
+            if (answers[4][1]) {
+                correctAnswerCount++;
+                document.getElementById('quizbox4').style = "box-shadow:0px 0px 5px #00cc99";
+                document.getElementById('quizboxe2').style = "background-color:none";
             } else if (!answers[4][1]) {
-                document.getElementById('quizbox4').style = "border: solid 2px red";
-                document.getElementById('quizboxe2').style = "background-color: green";
+                document.getElementById('quizbox4').style = "box-shadow:0px 0px 0px 2px red";
+                document.getElementById('quizboxe2').style = "background-color:#00cc99";
             }
         }
-
+        if (answers[5].includes(true)){
+            if (answers[5][2]) {
+                correctAnswerCount++;
+                document.getElementById('quizbox5').style = "box-shadow:0px 0px 5px #00cc99";
+                document.getElementById('quizboxf3').style = "background-color:none";
+            } else if (!answers[5][2]) {
+                document.getElementById('quizbox5').style = "box-shadow:0px 0px 0px 2px red";
+                document.getElementById('quizboxf3').style = "background-color:#00cc99; color:white";
+            }
+        }
+        if (answers[6].includes(true)){
+            if (answers[6][0]) {
+                correctAnswerCount++;
+                document.getElementById('quizbox6').style = "box-shadow:0px 0px 5px #00cc99";
+                document.getElementById('quizboxg1').style = "background-color:none";
+            } else if (!answers[6][0]) {
+                document.getElementById('quizbox6').style = "box-shadow:0px 0px 0px 2px red";
+                document.getElementById('quizboxg1').style = "background-color:#00cc99; color:white";
+            }
+        }
+        if (answers[7].includes(true)){
+            if (answers[7][0]) {
+                correctAnswerCount++;
+                document.getElementById('quizbox7').style = "box-shadow:0px 0px 5px #00cc99";
+                document.getElementById('quizboxh1').style = "background-color:none";
+            } else if (!answers[7][0]) {
+                document.getElementById('quizbox7').style = "box-shadow:0px 0px 0px 2px red";
+                document.getElementById('quizboxh1').style = "background-color:#00cc99; color:white";
+            }
+        }
+        if (answers[8].includes(true)){
+            if (answers[8][2]) {
+                correctAnswerCount++;
+                document.getElementById('quizbox8').style = "box-shadow:0px 0px 5px #00cc99";
+                document.getElementById('quizboxi3').style = "background-color:none";
+            } else if (!answers[8][2]) {
+                document.getElementById('quizbox8').style = "box-shadow:0px 0px 0px 2px red";
+                document.getElementById('quizboxi3').style = "background-color:#00cc99; color:white";
+            }
+        }
         if (answers[9].includes(true)){
-            if (answers[9][1])  {
-                document.getElementById('quizbox9').style = "border: solid 2px green";
-                document.getElementById('quizboxj2').style = "background-color: none";
+            if (answers[9][1]) {
+                correctAnswerCount++;
+                document.getElementById('quizbox9').style = "box-shadow:0px 0px 5px #00cc99";
+                document.getElementById('quizboxj2').style = "background-color:none";
             } else if (!answers[9][1]) {
-                document.getElementById('quizbox9').style = "border: solid 2px red";
-                document.getElementById('quizboxj2').style = "background-color: green";
+                document.getElementById('quizbox9').style = "box-shadow:0px 0px 0px 2px red";
+                document.getElementById('quizboxj2').style = "background-color:#00cc99; color:white";
             }
         }
-
+        // display do número de respostas corretas
+        if (correctAnswerCount === 0) {
+            document.getElementById('angryboi').style = "display:block"
+            document.getElementById('angryboi').innerHTML = "<b>Você não acertou uma questão! :'(</b>";
+        } else if (correctAnswerCount === 1) {
+            document.getElementById('angryboi').style = "display:block; color:#009999"
+            document.getElementById('angryboi').innerHTML = "<b>Você acertou uma questão!</b>"
+        } else if (correctAnswerCount === 10) {
+            document.getElementById('angryboi').style = "display:block; color:#009999"
+            document.getElementById('angryboi').innerHTML = "<b>Você acertou tudo! Tu é o bichão mermo!</b>"
+        } else if (correctAnswerCount > 1) {
+            document.getElementById('angryboi').style = "display:block; color:#009999"
+            document.getElementById('angryboi').innerHTML = "<b>Você acertou " + correctAnswerCount + " questões!</b>"
+        }
     }
-
-    console.log(answers);
 }
